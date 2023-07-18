889bed05aaa1d1f9830558e8393bf159fb197d5a
simone-caccialanza <simone.caccialanza@gmail.com>
2023-07-18 21:26:20 +0200

    Add scheduling and autoconfiguration

--- src/main/java/com/example/javaserver/JavaserverApplication.java
+++ src/main/java/com/example/javaserver/JavaserverApplication.java
@@ -1,9 +1,13 @@
 package com.example.javaserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAutoConfiguration
 public class JavaserverApplication {
 
 	public static void main(String[] args) {
