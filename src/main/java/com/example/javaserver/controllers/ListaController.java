c050ee66dbd43917fe3794d16633c9bc2daed0e8
simone-caccialanza <simone.caccialanza@gmail.com>
2023-10-25 04:51:16 +0200

    Add cross-origin all

--- src/main/java/com/example/javaserver/controllers/ListaController.java
+++ src/main/java/com/example/javaserver/controllers/ListaController.java
@@ -13,16 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class ListaController {
 
     private static final Logger logger = LoggerFactory.getLogger(ListaController.class);
