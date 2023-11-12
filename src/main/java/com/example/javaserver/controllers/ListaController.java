08c9a79e2fc306f624d47d2d0acb0dbe1b4d348e
simone-caccialanza <simone.caccialanza@gmail.com>
2023-11-12 01:27:02 +0100

    Add swagger ui and change Controller to RestController annotation for ListaController

--- src/main/java/com/example/javaserver/controllers/ListaController.java
+++ src/main/java/com/example/javaserver/controllers/ListaController.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +20,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 public class ListaController {
 
